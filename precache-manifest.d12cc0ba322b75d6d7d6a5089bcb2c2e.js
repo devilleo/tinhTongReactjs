72044ce607ce6b8a82ec36d9ff38ea2313571ca2
kha leo <lexuankha2409@gmail.com>
2019-09-08 18:48:10 +0700

    Updates

---
+++ precache-manifest.d12cc0ba322b75d6d7d6a5089bcb2c2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "055f2486aad02fb08bede2ae05f6fdcc",
+    "url": "/devilleo/tinhTongReactjs/index.html"
+  },
+  {
+    "revision": "d5f0576a592ec00c9219",
+    "url": "/devilleo/tinhTongReactjs/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "61be05b15be0b9f1e3ac",
+    "url": "/devilleo/tinhTongReactjs/static/css/main.31c6e1cf.chunk.css"
+  },
+  {
+    "revision": "d5f0576a592ec00c9219",
+    "url": "/devilleo/tinhTongReactjs/static/js/2.a1d5bba5.chunk.js"
+  },
+  {
+    "revision": "61be05b15be0b9f1e3ac",
+    "url": "/devilleo/tinhTongReactjs/static/js/main.d557e4e2.chunk.js"
+  },
+  {
+    "revision": "d9d1445a6498e65d0731",
+    "url": "/devilleo/tinhTongReactjs/static/js/runtime~main.dfa3cd7f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/devilleo/tinhTongReactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
